2c24511a0e2dfd2d1c0273f3af0dc25a2d710bbe
Rodrigo Antonio Caceres Vicencio <racacere@alumnos.inf.utfsm.cl>
2017-11-29 22:23:45 -0300

    added mailer files

---
+++ mailer.php
@@ -0,0 +1,54 @@
+<?php
+    // My modifications to mailer script from:
+    // http://blog.teamtreehouse.com/create-ajax-contact-form
+    // Added input sanitizing to prevent injection
+
+    // Only process POST reqeusts.
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Get the form fields and remove whitespace.
+        $name = strip_tags(trim($_POST["name"]));
+				$name = str_replace(array("\r","\n"),array(" "," "),$name);
+        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+        $message = trim($_POST["message"]);
+
+        // Check that data was sent to the mailer.
+        if ( empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            // Set a 400 (bad request) response code and exit.
+            http_response_code(400);
+            echo "Oops! Hubo un error en el envío. Por favor, completa el formulario y prueba de nuevo.";
+            exit;
+        }
+
+        // Set the recipient email address.
+        // FIXME: Update this to your desired email address.
+        $recipient = "walker.without.way@gmail.com";
+
+        // Set the email subject.
+        $subject = "Nuevo mensaje de $name, via myriammena.cl";
+
+        // Build the email content.
+        $email_content = "Nombre: $name\n";
+        $email_content .= "email: $email\n\n";
+        $email_content .= "Mensaje:\n$message\n";
+
+        // Build the email headers.
+        $email_headers = "De: $name <$email>";
+
+        // Send the email.
+        if (mail($recipient, $subject, $email_content, $email_headers)) {
+            // Set a 200 (okay) response code.
+            http_response_code(200);
+            echo "Gracias! Tu mensaje ha sido enviado.";
+        } else {
+            // Set a 500 (internal server error) response code.
+            http_response_code(500);
+            echo "Oops! Algo salio mal y no podemos enviar tu mensaje.";
+        }
+
+    } else {
+        // Not a POST request, set a 403 (forbidden) response code.
+        http_response_code(403);
+        echo "Hay un error con el envio, por favor intenta nuevamente.";
+    }
+
+?>
